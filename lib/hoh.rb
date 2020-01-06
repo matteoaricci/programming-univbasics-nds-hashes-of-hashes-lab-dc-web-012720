b49f985d77bea156949af03a069789ce9a872760
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-06 03:06:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -55,7 +55,7 @@ def naming_system
                 label: "Genus",
                 sub_category: {
                   label: "Species",
-                    sub_category: {nil}
+                    sub_category: nil
                 }
               }
             }
