2e52cce1cf4c410085f681d54e38096c28991bab
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-06 02:51:21 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,10 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
+  classification = {
+    
+    
+    
+  }
 
 end
